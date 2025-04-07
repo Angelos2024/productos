a1eb82fb311d4f897671334c6b00c06354f87abc
Angelos2024 <skyblackroan@gmail.com>
2025-04-07 16:22:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -247,7 +247,7 @@ async function rechazarProducto(index) {
   if (!producto) return;
 
   try {
-    const res = await fetch('https://verificador-api.vercel.app/api/verificador-api.js', {
+    const res = await fetch('https://productos-amber.vercel.app/api/verificador-api.js', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ accion: 'rechazar', producto })
