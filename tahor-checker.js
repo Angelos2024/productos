755f2d2318442674832044d9b727a66a1afd5d84
Angelos2024 <skyblackroan@gmail.com>
2025-07-01 15:11:12 -0600

    Update tahor-checker.js

--- tahor-checker.js
+++ tahor-checker.js
@@ -102,6 +102,7 @@ const ingredientesTame = [
 // 🛠️ Al inicio: genera lista normalizada una sola vez
 const ingredientesTameNormalizados = ingredientesTame.map(normalizeYsingularizar);
 
+
 function isTame(ingrediente) {
   const ingNormalizado = normalizeYsingularizar(ingrediente);
   const palabras = ingNormalizado.split(" ");
@@ -118,3 +119,6 @@ function analizarIngredientes(ingredientes) {
     ingredientesTame: impuros
   };
 }
+
+// Exportar al entorno global para que personal-main.js lo use también
+window.ingredientesTame = ingredientesTameNormalizados;
